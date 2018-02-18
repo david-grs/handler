3ad4deac1fd527d99956530d27904c328152629e
David Grs <david.a.grs@gmail.com>
2018-02-18 10:19:38 +0100

    formatting

--- handler.h
+++ handler.h
@@ -2,31 +2,33 @@
 
 #include <functional>
 
-struct [[nodiscard]] handler
+class [[nodiscard]] handler
 {
-    template <typename F>
-    explicit handler(F f)
-        : _deleter(std::forward<F>(f))
-    {}
-    handler() =default;
+public:
+	handler() =default;
 
-    ~handler() noexcept
-    {
-        if (_deleter)
-        {
-            _deleter();
-            _deleter = nullptr;
-        }
-    }
+	template <typename F>
+	explicit handler(F f) :
+		_deleter(std::forward<F>(f))
+	{}
 
-    handler(const handler&) =delete;
-    handler& operator=(const handler&) =delete;
+	~handler() noexcept
+	{
+		if (_deleter)
+		{
+			_deleter();
+			_deleter = nullptr;
+		}
+	}
 
-    handler(handler&&) =default;
-    handler& operator=(handler&&) =default;
+	handler(const handler&) =delete;
+	handler& operator=(const handler&) =delete;
+
+	handler(handler&&) =default;
+	handler& operator=(handler&&) =default;
 
 private:
-    std::function<void()> _deleter;
+	std::function<void()> _deleter;
 };
 
 
@@ -36,32 +38,35 @@ private:
 #include <algorithm>
 
 template <typename Callback>
-struct subscription_list
+class subscription_list
 {
-    handler add(Callback callback)
-    {
-        SubscriptionId allocated_id = _next_id++;
-        _subscriptions.emplace_back(allocated_id, std::move(callback));
+public:
+	handler add(Callback callback)
+	{
+		SubscriptionId allocated_id = _next_id++;
+		_subscriptions.emplace_back(allocated_id, std::move(callback));
 
-        return handler([this, allocated_id]()
-        {
-            _subscriptions.erase(std::remove_if(std::begin(_subscriptions), std::end(_subscriptions),
-                                                [&](const auto& p) { return p.first == allocated_id; }));
-        });
-    }
+		return handler([this, allocated_id]()
+		{
+			_subscriptions.erase(std::remove_if(std::begin(_subscriptions), std::end(_subscriptions),
+												[&](const auto& p) { return p.first == allocated_id; }));
+		});
+	}
 
-    template <typename... Args>
-    void call(Args&&... args)
-    {
-        for (auto&& f : _subscriptions)
-            f.second(std::forward<Args>(args)...);
-    }
+	template <typename... Args>
+	void call(Args&&... args)
+	{
+		for (auto&& f : _subscriptions)
+		{
+			f.second(std::forward<Args>(args)...);
+		}
+	}
 
-    auto size() const { return _subscriptions.size(); }
+	auto size() const { return _subscriptions.size(); }
 
 private:
-    using SubscriptionId = std::size_t;
+	using SubscriptionId = std::size_t;
 
-    SubscriptionId _next_id = {};
-    std::vector<std::pair<SubscriptionId, Callback>> _subscriptions;
+	SubscriptionId _next_id = {};
+	std::vector<std::pair<SubscriptionId, Callback>> _subscriptions;
 };
--- unit_tests.cc
+++ unit_tests.cc
@@ -8,136 +8,136 @@
 
 struct handler_test : public ::testing::Test
 {
-    handler subscribe()
-    {
-        int allocated_id = _next_id++;
-        _subscriptions.push_back(allocated_id);
+	handler subscribe()
+	{
+		int allocated_id = _next_id++;
+		_subscriptions.push_back(allocated_id);
 
-        return handler([this, allocated_id]()
-        {
-            std::cout << "removing id " << allocated_id << std::endl;
-            _subscriptions.erase(std::remove(std::begin(_subscriptions), std::end(_subscriptions), allocated_id));
-        });
-    }
+		return handler([this, allocated_id]()
+		{
+			std::cout << "removing id " << allocated_id << std::endl;
+			_subscriptions.erase(std::remove(std::begin(_subscriptions), std::end(_subscriptions), allocated_id));
+		});
+	}
 
 protected:
-    std::vector<int> _subscriptions;
-    int _next_id = 0;
+	std::vector<int> _subscriptions;
+	int _next_id = 0;
 };
 
 TEST_F(handler_test, basic)
 {
-    {
-        auto h = subscribe();
-        EXPECT_EQ(1, int(_subscriptions.size()));
-    }
+	{
+		auto h = subscribe();
+		EXPECT_EQ(1, int(_subscriptions.size()));
+	}
 
-    EXPECT_EQ(0, int(_subscriptions.size()));
+	EXPECT_EQ(0, int(_subscriptions.size()));
 }
 
 TEST_F(handler_test, move)
 {
-    {
-        handler moved;
+	{
+		handler moved;
 
-        {
-            auto h = subscribe();
-            moved = std::move(h);
-        }
+		{
+			auto h = subscribe();
+			moved = std::move(h);
+		}
 
-        EXPECT_EQ(1, int(_subscriptions.size()));
-    }
+		EXPECT_EQ(1, int(_subscriptions.size()));
+	}
 
-    EXPECT_EQ(0, int(_subscriptions.size()));
+	EXPECT_EQ(0, int(_subscriptions.size()));
 }
 
 TEST_F(handler_test, dtor)
 {
-    {
-        auto h = subscribe();
-    }
+	{
+		auto h = subscribe();
+	}
 
-    EXPECT_EQ(0, int(_subscriptions.size()));
+	EXPECT_EQ(0, int(_subscriptions.size()));
 }
 
 TEST_F(handler_test, multiple)
 {
-    handler h2;
+	handler h2;
 
-    {
-        auto h1 = subscribe();
-        h2 = subscribe();
-        EXPECT_EQ(2, int(_subscriptions.size()));
-    }
+	{
+		auto h1 = subscribe();
+		h2 = subscribe();
+		EXPECT_EQ(2, int(_subscriptions.size()));
+	}
 
-    EXPECT_EQ(1, int(_subscriptions.size()));
-    EXPECT_EQ(1, _subscriptions.back());
+	EXPECT_EQ(1, int(_subscriptions.size()));
+	EXPECT_EQ(1, _subscriptions.back());
 }
 
 
 
 struct subscription_list_test : public ::testing::Test
 {
-    using Callback = std::function<void(int)>;
+	using Callback = std::function<void(int)>;
 
-    handler subscribe(Callback callback)
-    {
-        return _subscriptions.add(callback);
-    }
+	handler subscribe(Callback callback)
+	{
+		return _subscriptions.add(callback);
+	}
 
 protected:
-    subscription_list<Callback> _subscriptions;
+	subscription_list<Callback> _subscriptions;
 };
 
 TEST_F(subscription_list_test, basic)
 {
-    int sum = 0;
+	int sum = 0;
 
-    {
-        auto h = subscribe([&](int i) { sum += i; } );
-        EXPECT_EQ(1, int(_subscriptions.size()));
-    }
+	{
+		auto h = subscribe([&](int i) { sum += i; } );
+		EXPECT_EQ(1, int(_subscriptions.size()));
+	}
 
-    EXPECT_EQ(0, int(_subscriptions.size()));
-    EXPECT_EQ(0, sum);
+	EXPECT_EQ(0, int(_subscriptions.size()));
+	EXPECT_EQ(0, sum);
 }
 
 TEST_F(subscription_list_test, call)
 {
-    int sum = 0;
+	int sum = 0;
 
-    auto h = subscribe([&](int i) { sum += i; } );
-    _subscriptions.call(1);
-    _subscriptions.call(3);
-    _subscriptions.call(5);
+	auto h = subscribe([&](int i) { sum += i; } );
+	_subscriptions.call(1);
+	_subscriptions.call(3);
+	_subscriptions.call(5);
 
-    EXPECT_EQ(9, sum);
+	EXPECT_EQ(9, sum);
 }
 
 
 TEST_F(subscription_list_test, call_after_destruction)
 {
-    int sum = 0;
+	int sum = 0;
 
-    {
-        auto h = subscribe([&](int i) { sum += i; } );
-        _subscriptions.call(1);
-    }
+	{
+		auto h = subscribe([&](int i) { sum += i; } );
+		_subscriptions.call(1);
+	}
 
-    _subscriptions.call(3);
-    _subscriptions.call(5);
+	_subscriptions.call(3);
+	_subscriptions.call(5);
 
-    EXPECT_EQ(1, sum);
+	EXPECT_EQ(1, sum);
 }
 
 TEST_F(subscription_list_test, discard_return)
 {
-    int sum = 0;
+	int sum = 0;
 
-    subscribe([&](int i) { sum += i; } );
-    _subscriptions.call(1);
-    _subscriptions.call(3);
-    _subscriptions.call(5);
+	subscribe([&](int i) { sum += i; } );
+	_subscriptions.call(1);
+	_subscriptions.call(3);
+	_subscriptions.call(5);
 
-    EXPECT_EQ(0, sum);
+	EXPECT_EQ(0, sum);
 }
